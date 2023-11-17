5fee94637d6cc54861d0bc5d5eef180a21b02ea5
InnaSofia <inna.artemieva.mironova@mail.ru>
2023-11-17 20:30:20 +0300

    доделано удаление продуктов

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -7,29 +7,51 @@ function App() {
 
   const [products, setProducts] = useState([
     {
+      id:1,
      number:1,
       name: 'Молоко'
     },
     {
+      id:2,
       number:2,
       name: 'Хлеб'
     },
     {
+      id:3,
       number:3,
       name: 'Кефир'
     },
     {
+      id:4,
       number:4,
       name: 'Конфеты'
     }
   ])
+
+  const deleteProduct = (id) => {
+    const filterdProducts = products.filter(product => product.id !== id)
+    setProducts(filterdProducts)
+  }
   return (
     <div>
    <Header />
-   {products.map((product) =>{
+   {products.length === 0 && (
+   <div className='text-6xl text-gray-500 text-center flex flex-col items-center mt-[100px]'>
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-20 h-20">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M20.25 7.5l-.625 10.632a2.25 2.25 0 01-2.247 2.118H6.622a2.25 2.25 0 01-2.247-2.118L3.75 7.5m6 4.125l2.25 2.25m0 0l2.25 2.25M12 13.875l2.25-2.25M12 13.875l-2.25 2.25M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125z" />
+</svg>
+
+    <p>Список покупок пуст</p>
+   </div>
+   )} 
+    {products.length > 0 && products.map((product) => {
            return(
-      <Products product={product}/>
-)})}
+      <Products key= {product.id} product={product} deleteProduct={deleteProduct}/>
+      
+)})
+
+}
+   
     
     </div>
   );}
--- src/components/Products/index.jsx
+++ src/components/Products/index.jsx
@@ -1,15 +1,31 @@
 import Button from '../Button/Button';
+import { BeakerIcon } from '@heroicons/react/24/solid'
 const Products = (props) => {
-const  { product } = props 
+const  { product, deleteProduct } = props 
+
+
 return(
     <div className='mb-5'>
-  <div className='max-w-xl mx-auto flex justify-between relative bg-white py-5 hover:bg-gray-50 px-4'>
-    <div>
-  <div className='text-lg font-medium text-gray-900'>{product.number}. {product.name}</div>
-  
-  </div><div><Button title='Добавлено!'/></div>
-  
-  </div></div>
+      
+                 <div className='max-w-xl mx-auto flex justify-between relative bg-white py-5 hover:bg-gray-50 px-4'>
+                       <div>
+                          <div className='text-lg font-medium text-gray-900'>
+                           {product.number}. {product.name}
+                         </div>
+                      </div>
+                      <div className='flex gap-10'>
+
+                          <div><Button title='Добавлено!'/></div>
+                          <a onClick={() => deleteProduct(product.id)} className='text-red-600 flex flex-col justify-around border text-center border-amber-600 border-solid rounded-md bg-red-100'><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-10 h-7">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M20.25 7.5l-.625 10.632a2.25 2.25 0 01-2.247 2.118H6.622a2.25 2.25 0 01-2.247-2.118L3.75 7.5m6 4.125l2.25 2.25m0 0l2.25 2.25M12 13.875l2.25-2.25M12 13.875l-2.25 2.25M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125z" />
+</svg>
+</a>
+                          
+                     </div>
+                    
+                     
+                </div>
+     </div>
 )
 }
 export default Products
